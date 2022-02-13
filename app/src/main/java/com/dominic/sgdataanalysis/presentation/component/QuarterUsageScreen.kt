0ae1f70ecb849a0fa83a828370706778490c80d3
Dominic Alexander <dominic@entrusttitle.com>
2022-02-13 20:19:21 +0530

    ui improvements

--- app/src/main/java/com/dominic/sgdataanalysis/presentation/component/QuarterUsageScreen.kt
+++ app/src/main/java/com/dominic/sgdataanalysis/presentation/component/QuarterUsageScreen.kt
@@ -1,16 +1,19 @@
 package com.dominic.sgdataanalysis.presentation.component
 
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Card
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.text.input.KeyboardType.Companion.Text
-import androidx.navigation.NavController
+
 import androidx.compose.material.Text
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.unit.dp
 import com.google.accompanist.pager.ExperimentalPagerApi
 import com.google.accompanist.pager.HorizontalPager
+import com.google.accompanist.pager.calculateCurrentOffsetForPage
 import com.google.accompanist.pager.rememberPagerState
-
+import kotlin.math.absoluteValue
 
 @ExperimentalPagerApi
 @Composable
@@ -21,10 +24,19 @@ fun QuarterUsageScreen (
 
     HorizontalPager(count = 10, state = pagerState) { page ->
         // Our page content
-        Text(
-            text = "Page: $page $year",
-            modifier = Modifier.fillMaxWidth()
-        )
+
+        Card(elevation = 4.dp, modifier = Modifier.fillMaxWidth()) {
+            Column(modifier = Modifier.padding(16.dp)) {
+
+                Text(text = "Year $year")
+                Spacer(modifier = Modifier.height(5.dp))
+                Text(
+                    text = "Page: $page $year",
+                    modifier = Modifier.fillMaxWidth()
+                )
+//                Text(text = "Consumption $usage")
+            }
+        }
     }
 
     LaunchedEffect(pagerState){
